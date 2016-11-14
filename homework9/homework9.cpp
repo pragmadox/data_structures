c04976c99ce95bdb0379bfe89b0777851634eca2
Jay Price <jared.x.price@gmail.com>
2016-11-14 14:46:17 -0500

    Changes to hoework9.

--- homework9/homework9.cpp
+++ homework9/homework9.cpp
@@ -28,9 +28,13 @@ struct structure
 
 int main()
 {
-    structure_pointer s;
-    s = get_structure(3, '*');
-    print_structure(s);
+    structure_pointer s1, s2, s3;
+    s1 = get_structure(3, '*');
+    s2 = get_structure(6, '$');
+    s3 = get_structure(9, '@');
+    print_structure(s1);
+    print_structure(s2);
+    print_structure(s3);
     return 0;
 }
 
@@ -50,6 +54,7 @@ structure_pointer get_structure(int n, char ch)
 void print_structure(structure_pointer s)
 {
     assert(s!=NULL);
+    cout << "----------------------" << endl;
     cout << "f1: " << s->f1 << endl;
     cout << "f2: " << s->f2 << endl;
     cout << "f3: " << s->f3 << endl;
